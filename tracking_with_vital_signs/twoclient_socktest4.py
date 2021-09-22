6d278666e3bee5f2c728e37605dffd24229235ac
bupt-dyi <35458876+NanJingNeo@users.noreply.github.com>
2021-09-22 11:58:30 +0800

    9.22

--- tracking_with_vital_signs/twoclient_socktest4.py
+++ tracking_with_vital_signs/twoclient_socktest4.py
@@ -3,7 +3,6 @@ from __future__ import print_function
 from pymoduleconnector import create_mc
 import pca_filter
 import numpy as np
-# import matlab.engine
 from dl_gj import *
 from pylab import *
 import threading
@@ -16,14 +15,7 @@ from time import sleep
 import logging
 import scipy.io as sio
 
-# def receive(tcpCliSock):
-#     ##这里改成非阻塞
-#     global data
-#     while True:
-#         buf = tcpCliSock.recv(1024).decode()
-#         tcpCliSock.send('ok'.encode())
-#         data = json.loads(buf)
-#         sleep(0.01)
+
 
 def receive(r_inputs, w_inputs, e_inputs):
     global data,refineDistance
@@ -169,11 +161,6 @@ if __name__=="__main__":
     BUFSIZ = 16
     ADDR = (HOST, PORT)
 
-    # frame_gen = sio.loadmat('frame_gen.mat')['frame_gen']
-    # eng = matlab.engine.start_matlab()
-    # mpl.rcParams['font.sans-serif'] = ['SimHei']
-    # eng.addpath('./ml')
-    # plt.ion()
     com = 'COM3'
 
     leiji = np.zeros((1, 748))#新雷达数据格式：747+1
@@ -182,10 +169,6 @@ if __name__=="__main__":
     data=dict()
     flag=1
     ycl=1
-
-    #这里需要修改
-    # ls1 = 4.26 #13
-    # ls2 = 0.95 #37
 
     #socket连接
     tcpCliSock = socket(AF_INET, SOCK_STREAM)
@@ -213,60 +196,12 @@ if __name__=="__main__":
             break
         sleep(0.05)
 
-
-    # ##用追踪时间戳对雷达，大概9s算一次
-    # while 1:
-    #     begin=time.time()
-    #     sleep(0.01)
-    #     ##这里改成对数据
-    #     if flag:
-    #         if data:
-    #             flag=0
-    #             begin = time.time()
-    #             s1=(data['1'][0]**2+data['1'][1]**2)**0.5
-    #             s2 = (data['2'][0] ** 2 + data['2'][1] ** 2) ** 0.5
-    #             timestamps=leiji[:,-1].copy()
-    #             index=np.where(timestamps.reshape(-1)==data['time'])
-    #             print(index)
-    #             if index:
-    #                 index=index[0].tolist()
-    #                 sliceindex=index[0]
-    #                 data=dict()
-    #                 # print(index)
-    #                 leiji=leiji[sliceindex:,:]
-    #     else:
-    #         if leiji.shape[0] >200:
-    #             flag=1
-    #             RawData1 = leiji[1:201, 0:-1].copy()
-    #             ###行前截10，列后截50，前截100
-    #             # 590,600
-    #             PureData1 = pca_filter.p_f(RawData1, M, L)
-    #
-    #             # 双人
-    #             peaks1_list, peaks2_list, tops1_list, tops2_list, s1, s2 = gj(PureData1, RawData1, M, L, s1, s2)
-    #             # peaks1_list, peaks2_list, tops1_list, tops2_list= gj(PureData1, RawData1, M, L)
-    #
-    #             result={'peaks1_list':peaks1_list,
-    #                     'peaks2_list': peaks2_list,
-    #                     'tops1_list': tops1_list,
-    #                     'tops2_list': tops2_list,
-    #                     }
-    #             with open('result.json','w') as f:
-    #                 json.dump(result,f)
-    #             f.close()
-    #             # sio.savemat(result,{'result':result})
-    #             logger.info(ycl)
-    #             ycl+=1
-    #             end=time.time()
-    #             logger.info(str(end-begin))
-
     ##用雷达时间戳对追踪
-    refineDistance=np.zeros([1,3])
+    refineDistance=np.zeros([1,3])#这只是初始化，有堆积的
     while 1:
         begin = time.time()
         sleep(0.01)
         ##这里改成对数据
-        # if flag:
         if leiji.shape[0] >210:
             flag=0
             RawData1 = leiji[-200:, :].copy()
@@ -289,7 +224,6 @@ if __name__=="__main__":
 
                 # 双人
                 peaks1_list, peaks2_list, tops1_list, tops2_list, s1, s2 = gj(PureData1, RawData1[:,0:-1], M, L, s1, s2)
-                # peaks1_list, peaks2_list, tops1_list, tops2_list= gj(PureData1, RawData1, M, L)
 
                 result={'peaks1_list':peaks1_list,
                         'peaks2_list': peaks2_list,
